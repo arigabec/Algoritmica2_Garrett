c4b61770677e80ff91bd490e77f1cce08975f1d7
Ariane Garrett <arigabec@gmail.com>
2021-08-23 19:03:57 -0400

    add primer parcial1

--- algoritmos/repaso/primer_parcial1.cpp
+++ algoritmos/repaso/primer_parcial1.cpp
@@ -24,6 +24,7 @@ int findGcd(int ss, int se, int qs, int qe, int si){
 }
 
 int findGcdRange(int ss, int se, int arr[],int n){
+    //En caso de que ingresemos números mayores o menores al tamaño del arreglo
     if (ss < 0 || se > n-1 || ss > se){
         cout << "No es valido" << endl;
         return -1;
@@ -32,6 +33,7 @@ int findGcdRange(int ss, int se, int arr[],int n){
 }
   
 int construirST(int arr[], int ss, int se, int si){
+    //Construimos el Segment Tree
     if (ss==se){
         st[si] = arr[ss];
         return st[si];
@@ -42,6 +44,7 @@ int construirST(int arr[], int ss, int se, int si){
 }
 
 int *init(int arr[], int n){
+    //Inicializamos el Segment Tree
    int peso = (int)(ceil(log2(n)));
    int size = 2*(int)pow(2, peso) - 1;
    st = new int[size];
@@ -50,9 +53,12 @@ int *init(int arr[], int n){
 }
 
 int main() {
+    //Arreglo con los valores
     int a[] = {2, 4, 5, 3, 6, 9};
+    //Tamaño del arreglo
     int n = sizeof(a)/sizeof(a[0]);
     init(a, n);
+    //Rango del intérvalo a sacar el gcd
     int l = 4;
     int r = 6;
     cout << "GCD: "<< findGcdRange(l -1, r -1, a, n) << endl;
