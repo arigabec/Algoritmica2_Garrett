da94f7dbd14da6ce9ecf04c5db15d49e87f2b550
Ariane Garrett <arigabec@gmail.com>
2021-08-24 15:53:50 -0400

    add problemas juez

--- problemas_jueces/contest1/problemaE.cpp
+++ problemas_jueces/contest1/problemaE.cpp
@@ -1,119 +1,48 @@
-//TRIE
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-string s[10010];
 
-bool check(string a, string b){
-    if(a.size() > b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        } 
-    }
-    return true;
-}
-
-int main(){
-    int cases, n;
-    cin >> cases;
-    while(cases--){
-        cin >> n;
-        for(int i = 0; i < n; i++){
-            cin >> s[i];
-        }
-        sort(s, s+n);
-        bool flag = true;
-        for(int i = 0; i < n; i++){
-            if(check(s[i], s[i + 1])){
-                flag = false;
-                break;
-            }
-        }
-        if(flag){
-            cout << "YES" << endl;
-        } else {
-            cout << "NO" << endl;
-        }
-    }
-    return 0;
-}
-
-
-
-
-
-
-/*bool flag;
+int sum[1000000];
 
 struct node {
-    char currentCharacter;       
-    bool isWord = false;
-    int number = 0;             
-    struct node *children[27]; 
-}*trie; 
+  int x;
+  int h;
+  int dp;
+  int id;
+} a[100000];
 
-void init() {
-    trie = new node();
-    flag = true;
+
+bool reverseSort(node node1,node node2){
+  return node1.x > node2.x;
 }
 
-bool insertWord(string word) {   
-    node *currentNode =  trie;  
-    for (int i = 0; i< word.length(); i++) {
-        int character = word[i] - 'a';      
-        if(currentNode->children[character] == NULL ) {
-            currentNode->children[character] = new node();
-        }
-        currentNode = currentNode->children[character];
-        currentNode->currentCharacter = word[i];
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x,h;
+        cin >> x >> h;
+        a[i].x = x;
+        a[i].h = h;
+        a[i].id = i;
     }
-    currentNode->isWord = true;
-}
-
-bool searchWord() {   
-    node *currentNode =  trie;  
-    for (int i = 0; i < 27; i++) {     
-        if(currentNode->isWord == true){
-           currentNode= currentNode->children[i+1];
-           if(currentNode->isWord == false){
-               return false;
-           }
-        }
+    sort(a, a + n, reverseSort);
+    for(int i = 0; i < n; i++){
+        a[i].dp = 1;
     }
-}
-
-void del(node *currentNode =  trie){ 
-    for(int i = 0; i < 20; i++){
-        if(currentNode->children[i] != NULL){
-            del(currentNode->children[i]);
-        }
-    }
-    delete(currentNode);
-}
-
-int main() {
-    int cases;
-    cin >> cases;
-    init();
-    while(cases--){
-        int numbers;
-        cin >> numbers;
-        while (numbers--){
-            string number;
-            cin >> number;
-            if(flag){
-                flag = insertWord(number);
+    for(int i = 1; i < n; i++){
+        for(int j = i - 1; j >= 0;j -= a[j].dp){
+            if(a[j].x > a[i].x + a[i].h - 1){
+                break;
             }
+            a[i].dp += a[j].dp;
         }
-        if(!flag){
-            cout << "NO" << endl;
-        } else {
-            cout << "YES" << endl;
-        }
-        del();
+    }
+    for(int i = 0; i < n; i++){
+      sum[a[i].id] = a[i].dp;
+    }
+    for(int i = 0; i < n; i++){
+        cout << sum[i] << " ";
     }
     return 0;
-}*/
+}
